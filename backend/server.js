98d93416891d934b67af03de15b8d8eb0fac172d
ajit-b-mali <ajitbm2003@gmail.com>
2026-01-19 01:54:03 +0530

    Ready for deployment

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Simple route to check if server is working
+app.get('/', (req, res) => res.send('Server is ready'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
